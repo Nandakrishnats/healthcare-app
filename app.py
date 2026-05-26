75f92fcc444b718acc533f9a648be7fa52e04f64
Nandakrishnats <nandakrishnats@gmail.com>
2026-05-26 11:59:33 +0000

    Fixed workflow configuration

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ limiter = Limiter(
 
 # MySQL Configuration
 
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_HOST'] = 'healthcare-db.cpkg4o86gmw2.ap-south-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = '3TVkuvtg5N'
+app.config['MYSQL_DB'] = 'healthcare_db'
 
 mysql = MySQL(app)
 
